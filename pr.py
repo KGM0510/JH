8bb70a590d10884c652d743c16879f5098e65629
KGM0510 <gunmin06051@gmail.com>
2022-04-10 01:38:14 +0900

    study2

---
+++ pr.py
@@ -0,0 +1,82 @@
+
+jumin = "990120-1234567"
+
+print ("성별 : " + jumin[7])
+print ("연 : " + jumin[0:2]) # 0 ~ 2 직전까지 (0, 1)
+print ("월 : " + jumin[2:4])
+print ("일 : " + jumin[4:6])
+
+print ("생년월일 : " + jumin[:6]) # 처음부터 6 직전까지
+print ("뒤 7자리 : " + jumin[7:]) # 7 부터 끝까지
+print ("뒤 7자리 : " + jumin[-7:]) # 7부터 끝까지
+
+python = "Python is Amazing"
+print (python.lower()) # 변수의 문자를 소문자로 출력
+print (python.upper()) # 변수의 문자를 대문자로 출력
+print (python[0].isupper()) # 변수의 x번째의 문자가 대문자면 true 소문자면 false
+print (len(python)) # 변수의 문자의 갯수 (띄어쓰기 포함)
+print (python.replace("Python", " Java")) # 변수에서의 문자를 다른 문자로 변경하여 출력
+
+index = python.index("n") # 변수에 어떤 문자가 몇 번째에 있는지 숫자로 출력
+print (index)
+index = python.index("n", index + 1) # 변수에 어떤 문자가 첫 번째에 나오는 것에 x 번째 다음 그 문자가 있는지 숫자로 출력
+print (index)
+
+print (python.find("Java")) # find는 원하는 문자가 없다면 -1
+#print (python.index(Java)) # index는 원하는 문자가 없다면 실행이 안됌
+print("hi")
+
+print (python.count("n")) # 변수 내에서 원하는 문자가 몇 번 나왔는지 출력
+
+# print ("a" + "b")
+# print ("a" , "b")
+
+# 방법 1
+print ("나는 %d살입니다." % 20 ) # "%d" % x x의 값을 d에 적용하여 출력
+print ("나는 %s을 좋아해요" % "파이썬") # "%s" % "X" X인 문자를 s에 적용하여 출력
+print("Apple 은 %c로 시작해요" % "A") # %c는 위와 다르게 한 글자만 출력 가능
+
+print ("나는 %s색과 %s색을 좋아해요." % ("파란", "빨간")) # "%s %s" % ("a" , "b") a와 b를 s에 순서대로 적용하여 출력
+
+print ("-"*50)
+
+# 방법 2
+print ("나는 {}살입니다.".format(20)) # "{}".format(x) {}안에 x를 적용하여 출력
+print ("나는 {}색과 {}색을 좋아해요.".format("파란", "빨간")) # "{} {}".format("a", "b") {}들에 a와 b를 순서대로 적용하여 출력
+print ("나는 {0}색과 {1}색을 좋아해요.".format("파란", "빨간")) # "{} {}".format("a", "b") {}들 안에 0~ 숫자를 넣어 그에 해당하는 문자를 적용하여 출력
+print ("나는 {1}색과 {0}색을 좋아해요.".format("파란", "빨간"))
+
+print ("-"*50)
+
+# 방법 3
+print ("나는 {age}살이며, {color}색을 좋아해요.".format(age = 17, color = "검정색"))
+print ("나는 {age}살이며, {color}색을 좋아해요.".format(color = "검정색", age = 17))
+
+print ("-"*50)
+
+# 방법 4 (파이썬 v3.6 이상~)
+age = 17
+color = "검정색"
+print(f"나는 {age}살이며, {color}색을 좋아해요.") # f"" f는 포맷인 듯
+
+print ("-"*50)
+
+# \n : 줄바꿈
+print("백문이 불여일견 \n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표
+# 저는 "KGM입니다."
+# print ('저는 "KGM"입니다')
+print ("저는 \"KGM\"입니다.")
+print ("저는 \'KGM\'입니다.")
+
+# \\ : 문장 내에서 \
+print ("C:\\Users\\user\\AppData\\Local\\Programs\\Python\\Python310-32\\python.exe c:\\kg/JH\\pr.py")
+# \r : 커서를 맨 앞으로 이동
+print ("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+print ("Redd\bApple")
+
+# \t : 탭
+print ("Red\tApple")
