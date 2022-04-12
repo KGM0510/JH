2852f0e01ab09a3aacb7f881fa02239add648cc4
KGM0510 <gunmin06051@gmail.com>
2022-04-12 22:27:43 +0900

    study2

--- pr.py
+++ pr.py
@@ -1,4 +1,7 @@
 
+from calendar import c
+
+
 jumin = "990120-1234567"
 
 print ("성별 : " + jumin[7])
@@ -80,3 +83,130 @@ print ("Redd\bApple")
 
 # \t : 탭
 print ("Red\tApple")
+
+print ("-"*50)
+
+# 리스트 []
+
+# 지하철 칸별로 10명, 20명, 30명
+subway1 = 10
+subway2 = 20
+subway3 = 30
+
+subway = [10 , 20, 30]
+print (subway)
+
+subway = ["유재석", "조세호", "박명수"]
+print (subway)
+
+# 조세호씨가 몇 번째 칸에 타고 있는가?
+print (subway.index("조세호"))
+
+# 하하씨가 다음 정류장에서 다음 칸에 탐
+subway.append("하하")
+print (subway)
+
+# 정형돈씨를 유재석 / 조세호 사이에 태워봄
+subway.insert(1, "정형돈")
+print (subway)
+
+# 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+print (subway.pop())
+print (subway)
+
+# print (subway.pop())
+# print (subway)
+
+# print (subway.pop())
+# print (subway)
+
+# 같은 이름의 사람이 몇 명 있는지 확인
+subway.append("유재석")
+print (subway)
+print (subway.count("유재석"))
+
+# insert : 리스트에 추가
+# pop : 뒤에서 부터 하나씩 제거
+
+print ("-"*50)
+
+# # 정렬도 가능
+# num_list = [5,2,4,3,1]
+# num_list.sort()
+# print (num_list)
+
+# # 순서 뒤집기 가능
+# num_list.reverse()
+# print (num_list)
+
+# # 모두 지우기
+# num_list.clear()
+# print (num_list)
+
+# 다양한 자료형 함께 사용
+num_list = [5,2,4,3,1]
+mix_list = ["조세호", 20, True]
+# print (mix_list)
+
+# 리스트 확장
+num_list.extend(mix_list)
+print (num_list)
+
+# sort : 리스트 정렬
+# reverse : 리스트 뒤에서부터 정렬
+# clear : 모두 지우기
+# A.extend(B) : 리스트 결합
+
+print ("-"*50)
+
+# cabinet = {3:"유재석" , 100:"김태호"}
+# print (cabinet[3])
+# print (cabinet[100])
+
+# print (cabinet[5]) # 안됌
+# print (cabinet.get(5, "사용 가능")) 
+
+# print (3 in cabinet) # True
+# print (5 in cabinet) # False
+
+cabinet = {"A-3":"유재석" , "B-100":"김태호"}
+print (cabinet["A-3"])
+print (cabinet["B-100"])
+
+# 새 손님
+print (cabinet)
+cabinet["A-3"] = "김종국"
+cabinet["C-20"] = "조세호"
+print (cabinet)
+
+# 간 손님
+del cabinet["A-3"]
+print (cabinet)
+
+# key 들만 출력
+print (cabinet.keys())
+
+# value 들만 출력
+print (cabinet.values())
+
+# key, value 쌍으로 출력
+print (cabinet.items())
+
+# 목용탕 폐점
+cabinet.clear()
+print (cabinet)
+
+# X = {key1:a, key2:b} 값 지정
+
+# X[key1] = c 다른걸로 변경 가능
+# X[key3] = d 새 key 추가 가능
+
+# key 들만 출력
+# print (cabinet.keys())
+
+# # value 들만 출력
+# print (cabinet.values())
+
+# # key, value 쌍으로 출력
+# print (cabinet.items())
+
